4cc6f89c3af5ce756a27d5ad825fb36328a09c6c
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2023-11-17 21:06:29 +0530

    Add files via upload

--- Asssignment-05/simple-multithreader.h
+++ Asssignment-05/simple-multithreader.h
@@ -4,26 +4,26 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <cstring>
-#include <chrono> //included extra can be removed used only for super precision of time
+#include <chrono> // used only for super precision of time in ms
 int user_main(int argc, char **argv);
 
-struct ThreadData
+struct ThreadData // information storing data-structure for thread dealing in 1D
 {
     std::function<void(int)> lambda;
     int low;
     int high;
 };
 
-struct ThreadData2D
+struct ThreadData2D // information storing data-structure for thread dealing in 2D
 {
     std::function<void(int, int)> lambda;
     int low1;
-    int high1;
     int low2;
+    int high1;
     int high2;
 };
 
-static void *parallel_for_helper(void *arg)
+static void *parallel_for_helper(void *arg) // calling the threads and lambda function to ease the execution of the threads 1D
 {
     ThreadData *data = static_cast<ThreadData *>(arg);
     for (int i = data->low; i < data->high; ++i)
@@ -33,7 +33,7 @@ static void *parallel_for_helper(void *arg)
     return nullptr;
 }
 
-static void *parallel_for_helper_2d(void *arg)
+static void *parallel_for_helper_2d(void *arg) // calling the threads and lambda function to ease the execution of the threads 2D
 {
     ThreadData2D *data = static_cast<ThreadData2D *>(arg);
     for (int i = data->low1; i < data->high1; ++i)
@@ -46,25 +46,36 @@ static void *parallel_for_helper_2d(void *arg)
     return nullptr;
 }
 
-void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads)
+void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads) //thread creater function for 1D arrays.
 {
     auto start = std::chrono::high_resolution_clock::now();
     std ::cout << "1D-Thread Creation Started\n";
-    // Create threads
+    // Creating threads and assigning values to the variables.
     pthread_t threads[numThreads];
     ThreadData data[numThreads];
-    int chunk = (high - low)/numThreads;
+    int chunk = (high - low) / numThreads;
     for (int i = 0; i < numThreads; ++i)
     {
-        data[i].low=i*chunk; data[i].high=(i+1)*chunk;
+        data[i].low = i * chunk;
+        data[i].high = (i + 1) * chunk;
         data[i].lambda = lambda;
-        pthread_create(&threads[i], nullptr, parallel_for_helper,(void*)&data[i]);
+        int check = pthread_create(&threads[i], nullptr, parallel_for_helper, (void *)&data[i]);
+        if (check != 0) // CHECKING if the thread created or not?
+        {
+            std::cerr << "Error creating thread: " << strerror(check) << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
     // Join threads
     for (int i = 0; i < numThreads; ++i)
     {
-        pthread_join(threads[i], nullptr);
+        int check = pthread_join(threads[i], nullptr); //waiting for the threads to complete the execution and return the values.
+        if (check != 0)//checking if perfect or not ??
+        {
+            std::cerr << "Error joining thread: " << strerror(check) << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
     std ::cout << "All 1D-Threads Completed\n";
@@ -82,19 +93,32 @@ void parallel_for(int low1, int high1, int low2, int high2, std::function<void(i
     // Create threads
     pthread_t threads[numThreads];
     ThreadData2D data[numThreads];
-    int chunk = (high1-low1)/numThreads;
+    int chunk = (high1 - low1) / numThreads;
     for (int i = 0; i < numThreads; ++i)
     {
-        data[i].low1=i*chunk; data[i].high1=(i+1)*chunk;
-        data[i].low2=low2; data[i].high2=high2;
+        // setting the important data of all the required values
+        data[i].low1 = i * chunk;
+        data[i].high1 = (i + 1) * chunk;
+        data[i].low2 = low2;
+        data[i].high2 = high2;
         data[i].lambda = lambda;
-        pthread_create(&threads[i], nullptr, parallel_for_helper_2d,(void*) &data[i]);
+        int check = pthread_create(&threads[i], nullptr, parallel_for_helper_2d, (void *)&data[i]); // creating threads for the 2D array taking i.
+        if (check != 0)                                                                             // CHEKING  if the thread actually created or not?
+        {
+            std::cerr << "Error creating thread: " << strerror(check) << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
     // Join threads
     for (int i = 0; i < numThreads; ++i)
     {
-        pthread_join(threads[i], nullptr);
+        int check = pthread_join(threads[i], nullptr); // joining all the created threads and waiting for all of them to complete their execution.
+        if (check != 0)// checking the return Bad or Good??
+        {
+            std::cerr << "Error joining thread: " << strerror(check) << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
     std ::cout << "All 2D-Threads Completed\n";
@@ -110,27 +134,38 @@ void parallel_for(int low1, int high1, int low2, int high2, std::function<void(i
 //     std ::cout << "2D-Thread Creation Started\n";
 
 //     // Create threads
-//     int loop_1 = (high1-low1);
+//     int loop_1 = (high1 - low1);
 //     pthread_t threads[numThreads * loop_1];
 //     ThreadData2D data[numThreads * loop_1];
-//     int chunk = (high2-low2)/numThreads;
+//     int chunk = (high2 - low2) / numThreads;
 //     int index = 0;
 //     for (int i = 0; i < loop_1; ++i)
 //     {
-//       for (int j = 0 ; j < numThreads ; j++)
-//       {
-//           data[index].low1=i; data[index].high1=(i+1);
-//           data[index].low2=j*chunk; data[index].high2=(j+1)*chunk;
-//           data[index].lambda = lambda;
-//           pthread_create(&threads[index], nullptr, parallel_for_helper_2d,(void*) &data[index]);
-//           index++;
-//       }
+//         for (int j = 0; j < numThreads; j++)
+//         {
+//             data[index].low1 = i;
+//             data[index].high1 = (i + 1);
+//             data[index].low2 = j * chunk;
+//             data[index].high2 = (j + 1) * chunk;
+//             data[index].lambda = lambda;
+//             int check = pthread_create(&threads[index], nullptr, parallel_for_helper_2d, (void *)&data[index]);
+//             if ( check != 0 ){
+//                 std::cerr << "Error creating thread: " << strerror(check) << std::endl;
+//                 exit(EXIT_FAILURE);
+//             }
+//             index++;
+//         }
 //     }
 
 //     // Join threads
-//     for (int i = 0; i < numThreads*loop_1; ++i)
+//     for (int i = 0; i < numThreads * loop_1; ++i)
 //     {
-//         pthread_join(threads[i], nullptr);
+//         int check = pthread_join(threads[i], nullptr);
+//         if (check != 0)
+//         {
+//             std::cerr << "Error joining thread: " << strerror(check) << std::endl;
+//             exit(EXIT_FAILURE);
+//         }
 //     }
 
 //     std ::cout << "All 2D-Threads Completed\n";
@@ -142,6 +177,7 @@ void parallel_for(int low1, int high1, int low2, int high2, std::function<void(i
 /* Demonstration on how to pass lambda as parameter.
  * "&&" means r-value reference. You may read about it online.
  */
+
 void demonstration(std::function<void()> &&lambda)
 {
     lambda();
@@ -157,6 +193,7 @@ int main(int argc, char **argv)
      * in the capture list. Only local variables must be
      * explicity captured if they are used inside lambda.
      */
+
     int x = 5, y = 1;
     // std ::cout<<"Hello Hi";
 
